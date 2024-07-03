915d679acbc5f59e692dcf52204e49fc7097618e
Hien Le <hien.le@saritasa.com>
2024-07-03 13:58:52 +0700

    Init new config

---
+++ lua/mappings.lua
@@ -0,0 +1,26 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map({"n", "v"}, "<C-d>", "<C-d>zz")
+map({"n", "v"}, "<C-u>", "<C-u>zz")
+map({"n", "v"}, "n", "nzzzv")
+map({"n", "v"}, "N", "Nzzzv")
+map({"n", "v"}, "G", "Gzz")
+map("n", "<leader>", "")
+
+map("v", "J", ":m '>+1<cr>gv=gv")
+map("v", "K", ":m '<-2<cr>gv=gv")
+map("v", "<", "<gv", { desc = "Indent line" })
+map("v", ">", ">gv", { desc = "Indent line" })
+
+local nomap = vim.keymap.del
+
+nomap("i", "<C-b>")
+nomap("i", "<C-e>")
+nomap("i", "<C-h>")
+nomap("i", "<C-l>")
+nomap("i", "<C-j>")
+nomap("i", "<C-k>")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,115 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+      vim.keymap.set("n", "<leader>lf", function()
+        vim.diagnostic.open_float { border = "rounded" }
+      end)
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "clangd",
+        "python-lsp-server",
+        "omnisharp",
+        "omnisharp-mono",
+        "markdown-oxide",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "asm",
+        "bash",
+        "c",
+        "c_sharp",
+        "cmake",
+        "comment",
+        "cpp",
+        "css",
+        "csv",
+        "cuda",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "haskell",
+        "haskell_persistent",
+        "hjson",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "json5",
+        "jsdoc",
+        "lua",
+        "luadoc",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "php",
+        "php_only",
+        "phpdoc",
+        "python",
+        "query",
+        "regex",
+        "scss",
+        "sql",
+        "sql",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      },
+    },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require "configs.indent-blankline"
+    end,
+  },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    lazy = false,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+    ft = function()
+      return require "configs.nvim-ts-autotag-filetype"
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    enabled = false,
+  },
+  {
+    "windwp/nvim-autopairs",
+    enabled = false,
+  },
+  {
+    "cohama/lexima.vim",
+    lazy = false,
+  },
+}
