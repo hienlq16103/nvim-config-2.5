7f2ea0c173bdb7243886308c324363b23fbac467
hienlq16103 <hienlq16103@gmail.com>
2024-12-01 10:46:24 +0700

    Add code action preview functionality

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -161,4 +161,11 @@ return {
     },
     event = "LspAttach",
   },
+  {
+    "aznhe21/actions-preview.nvim",
+    event = "LspAttach",
+    config = function()
+      vim.keymap.set({ "v", "n" }, "ap", require("actions-preview").code_actions)
+    end,
+  },
 }
