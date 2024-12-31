a0bd1f0d9b56d41c59bd3d2c8a9089f95a21e646
Hien Le <hien.le@saritasa.com>
2024-12-31 09:58:04 +0700

    Add buffer manager

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -153,5 +153,17 @@ return {
   {
     "mbbill/undotree",
     lazy = false,
-  }
+  },
+  {
+    "EL-MASTOR/bufferlist.nvim",
+    lazy = true,
+    keys = { { "<Leader>bm", ":BufferList<CR>", desc = "Open bufferlist" } },
+    dependencies = "nvim-tree/nvim-web-devicons",
+    cmd = "BufferList",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
