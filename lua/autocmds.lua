28599cdf510c9703c58d4e00511118346df7288c
Hien Le <hien.le@saritasa.com>
2026-05-22 13:50:19 +0700

    nvim 0.12 codelens migration

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -10,14 +10,6 @@ vim.api.nvim_create_user_command("DT", function()
   }
 end, { desc = "toggle diagnostic" })
 
-local lspAutoCmd = vim.api.nvim_create_augroup("lspAutoCmd", { clear = true })
-vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
-  callback = function()
-    vim.lsp.codelens.refresh { bufnr = 0 }
-  end,
-  group = lspAutoCmd,
-})
-
 -- vim.api.nvim_create_autocmd("FileType", { pattern = "qf", command = "wincmd J", group = lspAutoCmd })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
