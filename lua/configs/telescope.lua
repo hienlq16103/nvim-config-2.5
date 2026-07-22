cee80a107a74ed7a5c4d0780c2a4a7ba48aa363a
Hien Le <hien.le@saritasa.com>
2026-07-22 11:53:42 +0700

    Add delete buffer mapping for telescope

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,10 +1,21 @@
+local actions = require "telescope.actions"
+
 local otps = {
   defaults = {
     wrap_results = true,
     path_display = {
       "filename_first"
     }
-  }
+  },
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+        }
+      }
+    }
+  },
 }
 
 return otps
