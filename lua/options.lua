2bc96411964064e0ecae5d95854f24bc350bf4e0
Hien Le <hien.le@saritasa.com>
2026-09-14 16:08:10 +0700

    Use nu as neovim main shell

--- lua/options.lua
+++ lua/options.lua
@@ -2,9 +2,9 @@ require "nvchad.options"
 
 local o = vim.o
 
-o.shell = 'pwsh'
+o.shell = 'nu'
 o.shellxquote = ''
-o.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+o.shellcmdflag = '-c'
 o.shellquote = ''
 o.shellpipe = '| Out-File -Encoding UTF8 %s'
 o.shellredir = '| Out-File -Encoding UTF8 %s'
